d1f3d040a52989c89de15bb884ec809ea49dfc62
Valery Shvyndzikau <Valery_Shvyndzikau@epam.com>
2018-12-28 17:06:13 +0300

    Adjust unit tests flow

---
+++ src/app/articles/containers/articles.container.spec.js
@@ -0,0 +1,5 @@
+describe("Unit test running suite", () => {
+  it("Should be equal", () => {
+    expect(1).toEqual(2);
+  });
+});
--- webpack-test.config.js
+++ webpack-test.config.js
@@ -1,82 +1,78 @@
-const path = require('path');
-const webpack = require('webpack');
-const ROOT = path.resolve( __dirname, 'src' );
-const DESTINATION = path.resolve( __dirname, 'dist' );
+const path = require("path");
+const webpack = require("webpack");
+const ROOT = path.resolve(__dirname, "src");
+const DESTINATION = path.resolve(__dirname, "dist");
 
 /**
  * Webpack Plugins
  */
-const LoaderOptionsPlugin = require('webpack/lib/LoaderOptionsPlugin');
+const LoaderOptionsPlugin = require("webpack/lib/LoaderOptionsPlugin");
 
 module.exports = {
-    context: ROOT,
+  context: ROOT,
 
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
+  resolve: {
+    extensions: [".js"]
+  },
 
-    mode: 'development',
+  mode: "development",
 
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'tslint-loader',
-                    options: {
-                        emitErrors: true
-                    }
-                },
-                enforce: 'pre'
-            },
-            
-            {
-                test: /\.ts$/,
-                exclude: [ /node_modules/ ],
-                use: 'awesome-typescript-loader'
-            },
-            
-            {
-                test: /\.ts$/,
-                exclude: [ 
-                    /node_modules/,
-                    /\.spec\.ts$/
-                ],
-                use: 'istanbul-instrumenter-loader',
-                enforce: 'post'
-            },
+  module: {
+    rules: [
+      //   {
+      //     test: /\.ts$/,
+      //     exclude: /node_modules/,
+      //     use: {
+      //       loader: "tslint-loader",
+      //       options: {
+      //         emitErrors: true
+      //       }
+      //     },
+      //     enforce: "pre"
+      //   },
 
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            },
+      //   {
+      //     test: /\.ts$/,
+      //     exclude: [/node_modules/],
+      //     use: "awesome-typescript-loader"
+      //   },
 
-            {
-                test: /\.(jpg|png|gif|svg|woff|woff2|eot|ttf)$/,
-                use: 'file-loader'
-            },
+      //   {
+      //     test: /\.ts$/,
+      //     exclude: [/node_modules/, /\.spec\.ts$/],
+      //     use: "istanbul-instrumenter-loader",
+      //     enforce: "post"
+      //   },
 
-            {
-                test: /.html$/,
-                use: 'html-loader'
-            }
-        ]
-    },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
 
-    plugins: [
-        new LoaderOptionsPlugin({
-            debug: true,
-            options: {
-                tslint: {
-                    configuration: require('./tslint.json'),
-                    typeCheck: true
-                }
-            }
-        }),
+      {
+        test: /\.(jpg|png|gif|svg|woff|woff2|eot|ttf)$/,
+        use: "file-loader"
+      },
 
-    ],
+      {
+        test: /.html$/,
+        use: "html-loader"
+      }
+    ]
+  },
 
-    devtool: 'inline-source-map',
-    devServer: {}
+  plugins: [
+    // new LoaderOptionsPlugin({
+    //   debug: true,
+    //   options: {
+    //     tslint: {
+    //       configuration: require("./tslint.json"),
+    //       typeCheck: true
+    //     }
+    //   }
+    // })
+  ],
+
+  devtool: "inline-source-map",
+  devServer: {}
 };
